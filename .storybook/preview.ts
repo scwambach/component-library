f8d14ed7ee0ad9634c4be71b5a6d9803f3a3f4b2
Scott Wambach <scott@scottwamba.ch>
2024-05-08 14:40:55 -0500

    setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,7 @@
 import type { Preview } from "@storybook/react";
+import "./storybook.css";
+import "../src/styles/main.scss";
+import DocumentationTemplate from "./DocumentationTemplate.mdx";
 
 const preview: Preview = {
   parameters: {
@@ -7,6 +10,9 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    docs: {
+      page: DocumentationTemplate,
     },
   },
 };
